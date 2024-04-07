d6ec5a3a1c82482be6ce765c8f599d49f189c56e
Param Shah <paramshah.official@gmail.com>
2024-04-07 17:25:00 +0530

    Added repair mechanism and generated initial population

--- genetic-algorithm/genetic-algorithm.py
+++ genetic-algorithm/genetic-algorithm.py
@@ -1,6 +1,30 @@
 import numpy as np
+import random
+import parameters
 
-def generate_initial_population_v2(pop_size, jobs_operations_dict, num_machines):
+def repair_chromosome(chromosome, job_info):
+    def repair_sequence(sequence):
+        last_index = 0
+        for item in job_info.values():
+            sequence[last_index: last_index + len(item)] = sorted(sequence[last_index: last_index + len(item)])
+            last_index = last_index + len(item)
+
+        return sequence
+    def repair_assignment(assignment):
+        curr_ind = 0
+        for items in job_info.values():
+            for item in items:
+                machines = list(item.keys())
+                if assignment[curr_ind] not in machines:
+                    assignment[curr_ind] = random.choice(machines)
+                curr_ind = curr_ind + 1
+
+        return assignment
+
+    new_chromosome = {'assignment': repair_assignment(chromosome['assignment']), 'sequence': repair_sequence(chromosome['sequence'])}
+    return new_chromosome
+
+def generate_initial_population_v2(jobs_operations_dict, num_machines):
     """
     Generates an initial population for the genetic algorithm considering varied number of operations per job.
 
@@ -14,10 +38,14 @@ def generate_initial_population_v2(pop_size, jobs_operations_dict, num_machines)
       where 'assignment' is a list of machine assignments for each operation, and 'sequence' is a list of operations in the
       order they are to be processed.
     """
-    num_operations = sum(jobs_operations_dict.values())  # Total number of operations across all jobs
+    num_operations = 0  # Total number of operations across all jobs
+    for items in jobs_operations_dict.values():
+        num_operations = num_operations + len(items)
 
     # Initialize the population as a list
     population = []
+
+    pop_size =  parameters.POPULATION_SIZE
 
     # Fill in the population
     for _ in range(pop_size):
@@ -25,6 +53,8 @@ def generate_initial_population_v2(pop_size, jobs_operations_dict, num_machines)
             'assignment': np.random.randint(1, num_machines + 1, size=num_operations).tolist(),
             'sequence': np.random.permutation(np.arange(1, num_operations + 1)).tolist()
         }
+        chromosome = repair_chromosome(chromosome=chromosome, job_info=jobs_operations_dict)
+        chromosome['fitness'] = calculate_fitness(chromosome=chromosome, job_info=jobs_operations_dict)
         population.append(chromosome)
 
     return population
@@ -78,8 +108,28 @@ def calculate_fitness(chromosome, job_info):
     return time_taken
 
 
+def elitist_selection(population):
+    """
+    Selects the top-performing individuals from the population based on their fitness values using elitism.
+
+    Args:
+    - population: A list of dictionaries, where each dictionary represents a chromosome and contains
+                  'assignment', 'sequence', and 'fitness' keys.
+    - num_elites: The number of elite individuals to select for the next generation.
+
+    Returns:
+    - A list of dictionaries representing the elite chromosomes.
+    """
+    # Sort the population by the fitness value in ascending order (since lower fitness is better)
+    population = sorted(population, key=lambda x: x['fitness'])
+
+    # Select the top N elites
+    num_elites = parameters.POPULATION_SIZE
+    elites = population[:num_elites]
+
+    return elites
+
 # Example usage
-pop_size = 5
 jobs_operations_dict = {'J1': [{1: 2}, {1: 3, 2: 2}],
                         'J2': [{2: 1}, {1: 3}, {1: 4, 2: 2}],
                         'J3': [{1: 3, 2: 1}],
@@ -87,9 +137,11 @@ jobs_operations_dict = {'J1': [{1: 2}, {1: 3, 2: 2}],
 num_machines = 2
 
 # Generate the initial population
-# initial_population_v2 = generate_initial_population_v2(pop_size, jobs_operations_dict, num_machines)
-# for i, chromosome in enumerate(initial_population_v2, start=1):
-#     print(f"Chromosome {i}: {chromosome}")
+initial_population_v2 = generate_initial_population_v2(jobs_operations_dict, num_machines)
+print('Initial Population: ')
+for i, chromosome in enumerate(initial_population_v2, start=1):
+    print(f"Chromosome {i}: {chromosome}")
 
-chromosome = {'assignment': [1, 1, 2, 1, 2, 1], 'sequence': [1, 2, 3, 4, 5, 6]}
-print(calculate_fitness(chromosome=chromosome, job_info=jobs_operations_dict))
+# Test Chromosome
+# chromosome = {'assignment': [1, 1, 2, 1, 2, 1], 'sequence': [1, 2, 3, 4, 5, 6]}
+# print(calculate_fitness(chromosome=chromosome, job_info=jobs_operations_dict))
